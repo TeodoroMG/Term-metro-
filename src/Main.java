bd904ea392624bdd463976af6ba767887efcbd9c
TeodoroMG <108698165+TeodoroMG@users.noreply.github.com>
2024-04-10 09:25:35 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenio(a) Termometro");
+
+        System.out.println(" Termometro: Celsius a Fahrenheit");
+
+
+
+
+        double temperaturaEnCelsius = 30.4;
+        double temperaturaEnFahrenheit = (temperaturaEnCelsius * 1.8) + 32;
+
+        String mensaje = String.format("La temperatura de %f Celsius es equivalente a %f Fahrenheit", temperaturaEnCelsius, temperaturaEnFahrenheit);
+
+        System.out.println(mensaje);
+
+
+        int temperaturaEnFahrenheitEntero = (int) temperaturaEnFahrenheit;
+        System.out.println("La temperatura en Fahrenheit entera es: " + temperaturaEnFahrenheitEntero);
+
+
+    }
+}
+
